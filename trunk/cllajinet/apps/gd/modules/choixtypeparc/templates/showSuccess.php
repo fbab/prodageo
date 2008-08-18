f6ac8bb6eeb97f9b88fb6be2677e809ab7274f35
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2008-08-18 20:31:57 +0000

    Remplacement des appels a /var/www par sf_root_dir pour eviter accident de deploiement (dans envt de tests par exemple).

--- trunk/cllajinet/apps/gd/modules/choixtypeparc/templates/showSuccess.php
+++ trunk/cllajinet/apps/gd/modules/choixtypeparc/templates/showSuccess.php
@@ -1,4 +1,6 @@
-<?php require ('/var/www/cllajinet/lib/model/Findossier.php'); 
+<?php 
+// require ('/var/www/cllajinet/lib/model/Findossier.php'); 
+      require (sfConfig::get('sf_root_dir').'/lib/model/Findossier.php');
       $findossier=new Findossier();
       echo $_POST['typeparc'];
       $findossier->setVarTypeParc($_POST['typeparc']);
