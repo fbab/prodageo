c5086e19d475110ec3192e62e7778a7ed83f77c1
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2008-12-18 19:44:56 +0000

    Ouverture du projet de P. Secheresse

---
+++ trunk/calculatrice/src/java/Utilitaire/CalculatriceBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Utilitaire;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author root
+ */
+@Stateless
+public class CalculatriceBean implements CalculatriceRemote, CalculatriceLocal {
+
+    public int additionner(int a, int b) {
+        return 0;
+    }
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "EJB Methods > Add Business Method" or "Web Service > Add Operation")
+ 
+}
