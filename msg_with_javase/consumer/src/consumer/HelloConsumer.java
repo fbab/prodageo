3eb11eb006393fe8d3d4b186a4aa976da40ea6b0
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2008-12-19 07:18:17 +0000

    exemple sans MDB

---
+++ msg_with_javase/consumer/src/consumer/HelloConsumer.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package consumer;
+
+import javax.jms.*;
+
+/**
+ *
+ * @author root
+ */
+public class HelloConsumer {
+
+    
+    public HelloConsumer() {
+        try {
+            // creating a connection factory
+            // we are cheating here by not using jdni
+            ConnectionFactory cf= new com.sun.messaging.ConnectionFactory();
+
+            // create a connection
+            Connection connection = cf.createConnection();
+           
+            // create a session
+            Session session = connection.createSession( 
+                     false /* not transacted */, Session.AUTO_ACKNOWLEDGE);
+
+            // create destination HelloWorld
+            Destination destination = session.createQueue("HelloWorld");
+
+            // create a consumer
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // now that everything is ready to go, start the connection
+            connection.start();
+
+            // receive our message
+            TextMessage m = (TextMessage)consumer.receive();
+
+            System.out.println(m.getText());
+
+            // close everything
+            consumer.close();
+            session.close();
+            connection.close();
+
+            
+        } catch (JMSException ex) {
+            System.out.println("Error running program");
+            ex.printStackTrace();
+        }
+    }    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new HelloConsumer();        
+    }
+
+}
