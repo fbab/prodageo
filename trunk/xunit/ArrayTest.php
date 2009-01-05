a748086d34b3326eb13702a616711cac35be6178
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-05 23:40:35 +0000

    00h45

--- trunk/xunit/ArrayTest.php
+++ trunk/xunit/ArrayTest.php
@@ -11,6 +11,7 @@ require_once 'PHPUnit/Framework.php';
 // encore un autre a 23h32
 // encore un autre a 23h40
 // encore un autre a 00h27
+// encore un autre a 00h45
  
 class ArrayTest extends PHPUnit_Framework_TestCase
 {
