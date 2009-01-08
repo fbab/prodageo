6b9259d10122109dee8b02356aef9233ce4aaa22
matthieu.mika <matthieu.mika@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-08 10:41:45 +0000

    Commit via phing 2

--- trunk/xunit/mika/Enseignant.php
+++ trunk/xunit/mika/Enseignant.php
@@ -1,6 +1,10 @@
 <!--Tests de la classe Enseignant du projet LogIFIPS-->
 
 <?php
+/**
+*	Classe Enseignant
+*	@author Matthieu MIKA
+*/
 class Enseignant
 {
     private $_nom;
@@ -12,23 +16,37 @@ class Enseignant
         $this->_prenom = $prenom;
     }
 
+	/**
+	*	Fonction de mise a jour du nom
+	*/
     public function setNom($nom)
     {
         $this->_nom = $nom;
         return $this;
     }
 
+	/**
+	*	Fonction ecuperation du nom
+	*	@return String
+	*/
     public function getNom()
     {
         return $this->_nom;
     }
 
+	/**
+	*	Fonction de misà a jour du prenom
+	*/
     public function setPrenom($prenom)
     {
         $this->_prenom = $prenom;
         return $this;
     }
 
+	/**
+	*	Fonction de recuperation du prenom
+	*	@return String
+	*/
     public function getPrenom()
     {
         return $this->_prenom;
