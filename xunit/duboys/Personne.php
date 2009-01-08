8a3904f2816ea4f15c467bbd6f199bb91e7dee0a
grillet.adrien <grillet.adrien@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-08 10:36:31 +0000

    ajout de commentaires pour phpdoc

--- xunit/duboys/Personne.php
+++ xunit/duboys/Personne.php
@@ -4,23 +4,39 @@ class Personne
 private $_nom;
     private $_etage;
 
+    /**
+     * Constructeur
+     * @param string nom de la personne
+     */
     public function __construct($nom)
     {
         $this->_nom = $nom;
         $this->_etage = 0;
     }
 
+    /**
+     * Fonction setEtage
+     * @param int etage de la personne
+     */
     public function setEtage($etage)
     {
         $this->_etage = $etage;
     }
 
+    /**
+     * Fonction getEtage
+     * @return int etage de la personne
+     */
     public function getEtage()
     {
         return $this->_etage;
     }
 
-    public function getName()
+
+    /**
+     * Fonction getName
+     * @return string nom de la personne
+     */ public function getName()
     {
         return $this->_nom;
     }
