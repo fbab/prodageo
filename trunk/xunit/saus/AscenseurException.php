941ec20cac1d46c13169186b7e186de1a544448b
helene.saus <helene.saus@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-08 10:44:59 +0000

    modif AscenseurException

--- trunk/xunit/saus/AscenseurException.php
+++ trunk/xunit/saus/AscenseurException.php
@@ -2,4 +2,21 @@
 
 class AscenseurException {
 
+    private $_message;	
+
+    public function __construct($_message)
+    {
+        $this->_message = $_message;
+    } 
+
+    public function getMessage(){
+	return $this->_message;
+    }
+
+    public function __toString()
+    {
+        echo "Erreur : ". $this->getMessage();
+    }
 }
+
+?>
