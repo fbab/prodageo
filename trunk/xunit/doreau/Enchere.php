0694fd1167b5533ca577c734f9e3c997b3129dbc
pdoreau <pdoreau@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-08 10:39:21 +0000

    version presque finale

--- trunk/xunit/doreau/Enchere.php
+++ trunk/xunit/doreau/Enchere.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'Personne.php';
-//require_once 'AscenseurException.php'
+require_once 'EnchereException.php';
 
-class Enchere implements Countable 
+class Enchere 
 {
 	
 	private $_currentPersonne;
@@ -10,8 +10,7 @@ class Enchere implements Countable
 	private $_count;
 	private $_enchereSum;
 
-	public function __construct($price,$personne)
-	{
+	public function __construct($price,$personne){
 		$this->_currentPrice = $price;
 		$this->_currentPersonne = $personne;
 		$this->_count = 1;
@@ -27,10 +26,15 @@ class Enchere implements Countable
 	}
 
 	public function encherir($price,$personne){
-		$this->enchereSum += $price - $this->price; 
-		$this->_currentPrice = $price;
-		$this->_currentPersonne = $personne;
-		$this->_count++;
+		//echo $price." ".$this->_currentPrice;
+		if ($price < $this->_currentPrice){
+			$this->enchereSum += $price-$this->_currentPrice; 
+			$this->_currentPrice = $price;
+			$this->_currentPersonne = $personne;
+			$this->_count++;
+		}else{
+			throw new EnchereException("Le montant de la nouvelle enchere (".$price.") est inferieur ou egal a l'ancienne (".$this->_currentPrice.").");
+		}
 	}
 
 	public function getCount(){
@@ -38,45 +42,6 @@ class Enchere implements Countable
 	}
 
 	public function getAverage(){
-		return $this->enchereSum / $this->_count;
+		return $this->enchereSum/$this->$this->_count;
 	}
-
-    public function charger(Personne $p)
-    {
-        $this->_personnes->attach($p);
-    }
-
-    public function decharger(Personne $p)
-    {
-        $this->_personnes->detach($p);
-    }
-    
-    public function isEmpty()
-    {
-        return count($this) == 0;
-    }
-
-    public function go($a_l_etage)
-    {
-        $a_l_etage = abs((int)$a_l_etage);
-        if ($a_l_etage > self::MAX_ETAGES || $a_l_etage < 0) {
-            throw new AscenseurException('Etage invalide');
-        }	    
-        $this->_etage = $a_l_etage;
-    }
-
-    public function getName()
-    {
-        return $this->_nom;
-    }
-
-    public function getEtage()
-    {
-        return $this->_etage;
-    }
-	
-    public function count()
-    {
-        return count($this->_personnes);
-    }
 }
---
+++ trunk/xunit/doreau/EnchereException.php
@@ -0,0 +1,8 @@
+<?php
+class EnchereException  extends Exception {
+ 
+    public function __construct($msg) {
+        parent :: __construct($msg);
+    }    
+}
+?>
