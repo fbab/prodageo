0ee0dc61659f818302b66b7534b4caec74d65063
grillet.adrien <grillet.adrien@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-01-08 09:55:13 +0000

    changement fichiers php valide

---
+++ trunk/xunit/grillet/PersonneTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+require_once 'Personne.php';
+require_once 'PHPUnit/Framework.php';
+
+class PersonneTest extends PHPUnit_Framework_TestCase
+{
+    private $_personne;
+
+    protected function setUp()
+    {
+    	$this->_personne = new Personne('julien');
+    }
+
+    public function testPersonneInitialisee()
+    {
+        $this->assertEquals('julien',$this->_personne->getName());
+    }
+
+    public function testPersonneChangeEtage()
+    {
+        $this->_personne->setEtage(6);
+        $this->assertEquals(6,$this->_personne->getEtage());
+    }
+}
+
+?>
