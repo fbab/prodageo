3cdd8a9b6f5a7bf56dceee92420539765b3788d5
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2009-10-09 11:36:53 +0000

    Ajout de fonctions sur les Peer.

--- trunk/orm_propel/create.php
+++ trunk/orm_propel/create.php
@@ -5,7 +5,12 @@ include ( 'init.php' ) ;
 
 $author = new Author();
 $author->setFirstName("Jack");
-$author->setLastName("London");
+$author->setLastName("London1");
 $author->save();
+
+$c = new Criteria();
+$c->add(AuthorPeer::FIRST_NAME, "Jack");
+$i = BookPeer::doCount($c);
+print $i . "\n" ;
 
 ?>
