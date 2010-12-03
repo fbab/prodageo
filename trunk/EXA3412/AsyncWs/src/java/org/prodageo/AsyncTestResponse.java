392a220bbce6c4954d99e4960028debc9c14e877
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2010-12-03 14:28:38 +0000

    version avant ajout de @Stateless

---
+++ trunk/EXA3412/AsyncWs/src/java/org/prodageo/AsyncTestResponse.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.prodageo;
+
+/**
+ *
+ * @author fbaucher
+ */
+public interface AsyncTestResponse {
+    public void response( String msg );
+}
---
+++ trunk/EXA3412/AsyncWs/src/java/org/prodageo/AsyncTestResponseImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.prodageo;
+
+/**
+ *
+ * @author fbaucher
+ */
+import javax.jws.WebService;
+
+@WebService()
+public class AsyncTestResponseImpl implements AsyncTestResponse {
+
+    public void response(String msg) {
+    }
+} 
