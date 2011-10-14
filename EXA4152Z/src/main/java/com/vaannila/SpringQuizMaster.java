7715d60d16c85fb7d94cdd5f5d6f5fb19a1ba664
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2011-10-14 12:09:26 +0000

    Pour prendre en charge le refactor (1) et (2)

--- EXA4152Z/src/main/java/com/vaannila/SpringQuizMaster.java
+++ EXA4152Z/src/main/java/com/vaannila/SpringQuizMaster.java
@@ -2,7 +2,7 @@ package com.vaannila;
 
 	import javax.inject.Named;
 
-@Named("springQuizMaster")
+@Named("aQuizMaster")
 public class SpringQuizMaster implements QuizMaster {
 
 	public String popQuestion() {
