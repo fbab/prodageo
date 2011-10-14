1910041f83c06b518989a14f97e54fcb75a3dad2
fbaucher <fbaucher@643952d3-3053-0410-a3e0-9564d1c0a6b7>
2011-10-14 01:03:47 +0000

    loadBeanDefinitions n'applique pas @Inject => pour assurer une meme plomberie avec ou sans Inject, utiliser ClassPathXmlApplicationContext

--- trunk/EXA4152A/src/main/java/com/vaannila/QuizProgram.java
+++ trunk/EXA4152A/src/main/java/com/vaannila/QuizProgram.java
@@ -1,23 +1,13 @@
 package com.vaannila;
 
-import org.springframework.context.support.GenericApplicationContext ;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader ;
-import  org.springframework.core.io.ClassPathResource ;
-
-// import org.springframework.context.ApplicationContext;
-// import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class QuizProgram {
 	
 	public static void main(String[] args) {
 	
-	// ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		
-		    GenericApplicationContext context =
-		new GenericApplicationContext();
-    XmlBeanDefinitionReader xmlReader =
-		new XmlBeanDefinitionReader(context);
-    xmlReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		
 		QuizMasterService quizMasterService = (QuizMasterService) context.getBean("quizMasterService");
 		quizMasterService.askQuestion();
